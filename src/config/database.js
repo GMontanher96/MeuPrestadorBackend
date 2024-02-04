708fa3fcc7dd9b3b38eacbe8e9810ba182d89117
montanher <gabrielmontanherr@gmail.com>
2024-02-03 22:15:58 -0300

    Escopo inicial do projeto

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'myqsl',
+    host: 'localhost',
+    username: 'root',
+    password: '1996',
+    database: 'meuprestador',
+    define: {
+        timestamp: true,
+        underscored: true,
+    }
+}
